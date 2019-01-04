abc225aa7ed6200d3cef6b882ebbdef27fd09fcc
Nicholas Knight <38379829+Nickkick909@users.noreply.github.com>
2019-01-03 19:40:17 -0500

    Add files via upload

---
+++ BackToMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour {
+
+	public void OnClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Finish.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+public class Finish : MonoBehaviour {
+
+    public Collider player;
+
+    private void OnTriggerEnter(Collider player)
+    {
+        FindObjectOfType<GameManagerScript>().NextScene();
+    }
+
+}
